794c3fac9d7aea1d11e799684df8f6d2d409c7f8
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-10-13 22:30:31 +0800

    Enabled use of static files. Created and styled static index html page.

--- TheWorld/TheWorld/Startup.cs
+++ TheWorld/TheWorld/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -21,10 +16,15 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("<html><body><h3>Hello World!</h3></body></html>");
-            });
+            // Returning HTML content
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("<html><body><h3>Hello World!</h3></body></html>");
+            //});
+
+            // Using static files (Microsoft.AspNetCore.StaticFiles)
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
