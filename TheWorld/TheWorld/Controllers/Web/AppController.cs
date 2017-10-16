07b0d8882bf3372d1ca8c9abbe17e9bdc238ae02
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-10-16 22:49:36 +0800

    Added model state validation, extracting configuration, etc.

--- TheWorld/TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/TheWorld/Controllers/Web/AppController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using TheWorld.Services;
 using TheWorld.ViewModels;
 
@@ -7,10 +8,12 @@ namespace TheWorld.Controllers.Web
     public class AppController : Controller
     {
         private IMailService _mailService;
+        private IConfigurationRoot _config;
 
-        public AppController(IMailService mailService)
+        public AppController(IMailService mailService, IConfigurationRoot config)
         {
             _mailService = mailService;
+            _config = config;
         }
 
         public IActionResult Index()
@@ -26,7 +29,18 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
-            _mailService.SendMail("jojo@sardez.com", model.Email, "From TheWorld", model.Message);
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("Email", "We don't support AOL addresses");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
             return View();
         }
 
--- TheWorld/TheWorld/Startup.cs
+++ TheWorld/TheWorld/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TheWorld.Services;
@@ -9,16 +10,26 @@ namespace TheWorld
     public class Startup
     {
         private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
 
         public Startup(IHostingEnvironment env)
         {
             _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
+
             // Dependency injection
             if (_env.IsDevelopment())
             {
