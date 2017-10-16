ce36d10e8bbf362d44459911c5300e3020c3115e
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-10-16 23:16:48 +0800

    Added bootstrap and font awesome.

--- TheWorld/TheWorld/wwwroot/js/site.js
+++ TheWorld/TheWorld/wwwroot/js/site.js
@@ -23,14 +23,19 @@
     //    });
 
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa");
 
     $("#sidebarToggle").on("click",
         function() {
             $sidebarAndWrapper.toggleClass("hide-sidebar");
             if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-                $(this).text("Show Sidebar");
+                //$(this).text("Show Sidebar");
+                $icon.removeClass("fa-angle-left");
+                $icon.addClass("fa-angle-right");
             } else {
-                $(this).text("Hide Sidebar");
+                //$(this).text("Hide Sidebar");
+                $icon.addClass("fa-angle-left");
+                $icon.removeClass("fa-angle-right");
             }
         });
 
