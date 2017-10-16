76a4609460bce53ab3b43812f1c0ab4f8b7fe297
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-10-16 22:00:49 +0800

    Contact page implementation. Added view model, data annotation, and jQuery client side validation.

---
+++ TheWorld/TheWorld/ViewModels/ContactViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheWorld.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(4096, MinimumLength = 10)]
+        public string Message { get; set; }
+    }
+}
