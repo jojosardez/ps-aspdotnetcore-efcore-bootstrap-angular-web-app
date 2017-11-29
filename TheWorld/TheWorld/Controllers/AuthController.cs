a38d31686d10edcc57350a6d1979956c413bdd10
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-11-29 17:35:12 +0800

    Added Login view and Auth controller.

---
+++ TheWorld/TheWorld/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheWorld.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+    }
+}
