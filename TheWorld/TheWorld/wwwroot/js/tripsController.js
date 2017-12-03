5afe048fa9b8fd664f48f21c9fa17038a3bb51a6
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-12-03 19:03:08 +0800

    Added getting and saving items from the API via angular controller.

--- TheWorld/TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/TheWorld/wwwroot/js/tripsController.js
@@ -1,5 +1,5 @@
 ﻿// tripsController.js
-(function () {
+(function() {
 
     "use strict";
 
@@ -7,18 +7,44 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         var vm = this;
-        vm.trips = [
-            {
-                name: "US Trip",
-                created: new Date()
-            },
-            {
-                name: "World Trip",
-                created: new Date()
-            }
-        ];
+        vm.trips = [];
+
+        vm.newTrip = {};
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function(response) {
+                    // Success
+                    angular.copy(response.data, vm.trips);
+                },
+                function(error) {
+                    //Failure
+                    vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function() {
+                vm.isBusy = false;
+            });
+
+        vm.addTrip = function () {
+            vm.isBusy = true;
+            vm.errorMessage = "";
+            $http.post("/api/trips", vm.newTrip).then(function (response) {
+                        // Success
+                vm.trips.push(response.data);
+                        vm.newTrip = {};
+                    },
+                    function (error) {
+                        //Failure
+                        vm.errorMessage = "Failed to save new trips: " + error;
+                    })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+        };
     }
 
 })();
